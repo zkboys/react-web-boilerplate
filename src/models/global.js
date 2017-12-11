2e93fd154ab04b9f2f2885830ad919c6377f09c5
zkboys <876213774@qq.com>
2017-12-11 13:51:38 +0800

    做测试

--- src/models/global.js
+++ src/models/global.js
@@ -1,7 +1,17 @@
+import {getCurrentLoginUser} from '../commons';
+
+const currentLoginUser = getCurrentLoginUser();
+/**
+ * 存放系统级数据 及 操作
+ */
 export default {
     initialState: {
         loading: false,
+        loginUser: currentLoginUser,
+        permissions: currentLoginUser.permissions,
     },
+    setLoginUser: (state, {payload}) => ({loginUser: payload}),
+    setPermissions: (state, {payload}) => ({permissions: payload}),
     showLoading: () => ({loading: true}),
     hideLoading: () => ({loading: false}),
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,32 @@
 import React, {Component} from 'react'
+import {connect} from "../../models/index";
 import './style.less'
 
+@connect(state => ({menus: state.menu.menus, loginUser: state.global.loginUser}))
 export default class Home extends Component {
     componentWillMount() {
-        // home 直接跳转
-        this.props.history.replace('/dashboard/analysis');
+        console.log(this.props.loginUser);
+        // home 直接跳转 到系统第一个可用菜单页面
+        const {menus} = this.props;
+        if (menus && menus.length) {
+            let path;
+            for (let i = 0; i < menus.length; i++) {
+                const m = menus[i];
+                if (m.path) {
+                    path = m.path;
+                    break;
+                }
+            }
+            if (path) {
+                console.log(path);
+                this.props.history.replace(path);
+            }
+        }
     }
 
     render() {
         return (
-            <div>Home</div>
+            <div/>
         );
     }
 }
